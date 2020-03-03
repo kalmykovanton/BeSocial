30cc79d7a9d89d495c9e348eb462fadb8f8545b4
KonstantinKuksov <kkuksov@gmail.com>
2020-03-03 10:05:16 +0200

    add react-router-dom and make Navbar switching

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Footer from "./components/Footer/Footer";
 import Dialogs from "./components/Dialogs/Dialogs";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 const App = () => {
 	return (
-		<div className={"app-wrapper"}>
-			<Header/>
-			<Navbar/>
-			<div className={"app-wrapper-content"}>
-				<Dialogs/>
-				{/*<Profile/>*/}
+		<BrowserRouter>
+			<div className={"app-wrapper"}>
+				<Header/>
+				<Navbar/>
+				<div className={"app-wrapper-content"}>
+					<Route path={'/profile'} component={Profile}/>
+					<Route path={'/dialogs'} component={Dialogs}/>
+					<Route path={'/news'} component={News}/>
+					<Route path={'/music'} component={Music}/>
+					<Route path={'/settings'} component={Settings}/>
+				</div>
+				<Footer/>
 			</div>
-			<Footer/>
-
-		</div>
+		</BrowserRouter>
 	);
 }
 
