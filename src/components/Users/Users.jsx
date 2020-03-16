b1e71c474202c2cd023b228f9d4c0e2e2195671c
KonstantinKuksov <kkuksov@gmail.com>
2020-03-16 12:22:58 +0200

    refactor: refactoring Users to class component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,55 +3,57 @@ import styles from './Users.module.css';
 import * as axios from 'axios';
 import userPhoto from './../../img/user1.png';
 
-const Users = (props) => {
+class Users extends React.Component {
 
-    if ( props.users.length === 0 ) {
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then( response => {
-                props.setUsers(response.data.items);
+                this.props.setUsers(response.data.items);
             });
     }
 
-    return (
-        <div className={styles.container}>
-            {
-                props.users.map(user => <div className={styles.user} key={user.id}>
-                    <div className={styles.avatar}>
-                        <div>
-                            <img className={styles.img}
-                                 src={ user.photos.small ? user.photos.small : userPhoto } alt="ava"/>
+    render() {
+        return (
+            <div className={styles.container}>
+                {
+                    this.props.users.map(user => <div className={styles.user} key={user.id}>
+                        <div className={styles.avatar}>
+                            <div>
+                                <img className={styles.img}
+                                     src={ user.photos.small ? user.photos.small : userPhoto } alt="ava"/>
+                            </div>
+                            <div>
+                                { user.followed
+                                    ? <button onClick={ () => { this.props.unfollow(user.id) } }
+                                              className={styles.followButton}>Unfollow</button>
+                                    : <button onClick={ () => { this.props.follow(user.id) } }
+                                              className={styles.followButton}>Follow</button>}
+                            </div>
                         </div>
-                        <div>
-                            { user.followed
-                                ? <button onClick={ () => { props.unfollow(user.id) } }
-                                          className={styles.followButton}>Unfollow</button>
-                                : <button onClick={ () => { props.follow(user.id) } }
-                                          className={styles.followButton}>Follow</button>}
+                        <div className={styles.info}>
+                            <div className={styles.name}>
+                                {user.name}
+                            </div>
+                            <div>
+                                {user.status}
+                            </div>
                         </div>
-                    </div>
-                    <div className={styles.info}>
-                        <div className={styles.name}>
-                            {user.name}
+                        <div className={styles.location}>
+                            <div>
+                                City: {"user.location.city"}
+                            </div>
+                            <div>
+                                Country: {"user.location.country"}
+                            </div>
                         </div>
-                        <div>
-                            {user.status}
-                        </div>
-                    </div>
-                    <div className={styles.location}>
-                        <div>
-                            City: {"user.location.city"}
-                        </div>
-                        <div>
-                            Country: {"user.location.country"}
-                        </div>
-                    </div>
-                </div>)
-            }
-            <div className={styles.buttonContainer}>
-                <button className={styles.moreButton}>More Users</button>
+                    </div>)
+                }
+                <div className={styles.buttonContainer}>
+                    <button className={styles.moreButton}>More Users</button>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Users;
