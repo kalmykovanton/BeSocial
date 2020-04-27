15ca4d13a165ff6897124ba263a89cba2f664793
KonstantinKuksov <kkuksov@gmail.com>
2020-04-27 17:27:08 +0300

    refactor: use HashRouter instead ИкщÑBrowserRouter in index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import registerServiceWorker from './registerServiceWorker';
 import store from './redux/ReduxStore';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import App from './App';
 import './my.css';
 import {Provider} from 'react-redux';
 
 ReactDOM.render(
-        <BrowserRouter>
+        <HashRouter>
             <Provider store={store}>
                 <App />
             </Provider>
-        </BrowserRouter>,
+        </HashRouter>,
         document.getElementById('root'));
 
 registerServiceWorker();
